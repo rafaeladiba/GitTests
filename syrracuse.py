dd8c315a94a3309f455bfd4e00290b926033f659
name test <test.mail>
2022-03-01 12:03:14 +0100

    pulkk

---
+++ syrracuse.py
@@ -0,0 +1,11 @@
+def syracuse():
+    
+    u=int(input())
+    while u!=1:# pour i allant de 1 à n
+        if u%2==0:   # u%2: reste de la division euclidienne de u par 2
+            u = u//2  # u//2: quotient de la division euclidienne de  u par 2
+        else:
+            u = 3*u+1
+        print(u)
+        
+    
