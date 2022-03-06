84dd318fd35c92388f41ad63c686e900a45da78a
rafael adiba <rafaeladiba@iMac-de-Rafael-ADIBA.local>
2022-03-06 17:28:47 +0100

    modifs

--- syrracuse.py
+++ syrracuse.py
@@ -3,7 +3,7 @@ def syrracuse():
     u=int(input("Mettez une valeur: "))
     liste = []
     liste.append(u)
-    while u!=1:# tant que u différent de 1
+    while u!=1:# tant que u different de 1
         if u%2==0:   # u%2: reste de la division euclidienne de u par 2
             u = u//2  # u//2: quotient de la division euclidienne de  u par 2
         else:
